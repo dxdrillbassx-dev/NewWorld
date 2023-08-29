c34110022639bd40f9c672879f5a68929b1e508b
sobr228 <askerka110038302@gmail.com>
2023-08-29 05:25:28 +0300

    v.0.0.2

---
+++ src/main/java/org/dxdrillbassx/newworld/Signature.java
@@ -0,0 +1,9 @@
+package org.dxdrillbassx.newworld;
+
+import org.bukkit.ChatColor;
+
+public class Signature {
+    public static String MAIN = ChatColor.WHITE + "[NewWorld]: ";
+    public static String ERROR = ChatColor.RED + "[ERROR]: ";
+
+}
