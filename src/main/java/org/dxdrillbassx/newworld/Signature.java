19278d118a6aa2cf1e6f15863940f2f062f603c1
DXDRILLBASSX <61717964+sobr228@users.noreply.github.com>
2023-08-29 05:29:11 +0300

    v.0.0.2

--- src/main/java/org/dxdrillbassx/newworld/Signature.java
+++ src/main/java/org/dxdrillbassx/newworld/Signature.java
@@ -4,6 +4,6 @@ import org.bukkit.ChatColor;
 
 public class Signature {
     public static String MAIN = ChatColor.WHITE + "[NewWorld]: ";
-    public static String ERROR = ChatColor.RED + "[ERROR]: ";
+    public static String ERROR = MAIN + ChatColor.RED + "[ERROR]: ";
 
 }
