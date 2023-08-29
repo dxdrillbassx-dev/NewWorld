6acdf31da93704dd089e2f8f9ec27fc8817400a3
sobr228 <askerka110038302@gmail.com>
2023-08-29 05:02:52 +0300

    Initial commit

---
+++ src/main/java/org/dxdrillbassx/newworld/NewWorld.java
@@ -0,0 +1,17 @@
+package org.dxdrillbassx.newworld;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NewWorld extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
