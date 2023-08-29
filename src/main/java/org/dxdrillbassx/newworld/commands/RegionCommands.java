e5dc2a40adda0403f56aade09a4ff844284511d2
sobr228 <askerka110038302@gmail.com>
2023-08-29 05:16:28 +0300

    v.0.0.1

---
+++ src/main/java/org/dxdrillbassx/newworld/commands/RegionCommands.java
@@ -0,0 +1,19 @@
+package org.dxdrillbassx.newworld.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class RegionCommands implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if (!(sender instanceof Player)){
+            System.out.println("Только игрок может использовать эту команду!");
+        }
+
+        if ()
+        return false;
+    }
+}
